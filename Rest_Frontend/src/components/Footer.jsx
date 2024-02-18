8d69166071a131cb510a93f77b4f6572450e3f77
Ajay45verma <ajay2verma.2001@gmail.com>
2024-02-19 01:42:51 +0530

    first commit

---
+++ Rest_Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Ajay</div>
+          <div className="right">
+            <p>Institute of Engineering and Technology, Lucknow</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By AJAY VERMA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Ajay Verma.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
